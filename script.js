9f48d12501adcc6ffd819dcd4aebf40c2c88695f
creative-username <myc82570@gmail.com>
2022-03-05 21:36:35 +0200

    added functions to prompt for the player's choice and create a computer's choice

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function convertMove(move) {
             return 'scissor'
     }
 }
+
+function playerPlay() {
+    let move = parseInt(prompt(`Insert the the number for the move you wish to make:
+            [1] Rock
+            [2] Paper
+            [3] Scissors`, 1))
+
+    return convertMove(move)
+}
+
+function computerPlay() {
+    let move = parseInt(Math.random() * 3) + 1
+
+    return convertMove(move)
+}
