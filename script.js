0424698d5d84ee10d272f6e5954003685f6d9efa
creative-username <myc82570@gmail.com>
2022-03-05 21:33:45 +0200

    added a convertMove function to conveert the players' choices to valid moves

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Hello! Welcome to the rock -> paper -> scissors game')
+
+function convertMove(move) {
+    switch (move) {
+        case 1:
+            return 'rock'
+            break
+
+        case 2:
+            return 'paper'
+            break
+        
+        case 3:
+            return 'scissor'
+    }
+}
